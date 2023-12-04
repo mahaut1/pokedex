a883c923ee88cdad0c5382b747d5b3a1de8c3398
Mahaut Windal <mwindal@hotmail.com>
2023-12-04 12:42:32 +0100

    Header PokemonList

---
+++ src/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Header.css'
+import Logo from './Logo/Logo'
+
+function Header(){
+   return(<header>Mon header</header>)
+}
+
+export default Header
---
+++ src/PokemonList/PokemonList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './PokemonList.css'
+function PokemonList(){
+ return(<div><p>Ma Pokemon List</p></div>)
+}
+
+export default PokemonList
